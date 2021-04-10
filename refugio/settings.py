6a6102287ad6028e09527b70e3b5afaf88784479
alfonso <@lfareiza1989>
2021-04-10 19:58:23 -0300

    corregido path de apps

--- refugio/settings.py
+++ refugio/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'refugio.mascota',
-    'refugio.adopcion',
+    'refugio.apps.mascota',
+    'refugio.apps.adopcion',
 ]
 
 MIDDLEWARE = [
