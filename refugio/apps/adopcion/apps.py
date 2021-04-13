ad52a773db6c9178d76f80af98ebe130edfffbac
alfonso <@lfareiza1989>
2021-04-13 10:23:39 -0300

    corregido error con este archivo. ahora se envía path de app dentro del nombre de la app

--- refugio/apps/adopcion/apps.py
+++ refugio/apps/adopcion/apps.py
@@ -1,6 +1,6 @@
-# from django.apps import AppConfig
+from django.apps import AppConfig
 
 
-# class AdopcionConfig(AppConfig):
-#     default_auto_field = 'django.db.models.BigAutoField'
-#     name = 'adopcion'
+class AdopcionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'refugio.apps.adopcion'
