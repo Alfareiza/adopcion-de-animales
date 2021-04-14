fe51204266160aa7aad7e8d1ef80a96f6f662a5d
alfonso <@lfareiza1989>
2021-04-13 23:09:11 -0300

    Creado modudlo de formulario para registrar una adopción

---
+++ refugio/apps/mascota/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+
+from refugio.apps.mascota.models import Mascota
+
+
+class MascotaForm(forms.ModelForm):
+    class Meta:
+        model = Mascota
+        fields = ['nombre', 'sexo', 'edad_aproximada',
+                  'fecha_de_rescate', 'raza', 'comentarios', 'persona',
+                  'vacuna']
+        labels = {'nombre': 'Nombre',
+                  'sexo': 'Sexo',
+                  'edad_aproximada': 'Edad Aproximada',
+                  'fecha_de_rescate': 'Fecha de Rescate',
+                  'raza': 'Raza',
+                  'comentarios': 'Comentarios',
+                  'persona': 'Adoptante',
+                  'vacuna': 'Vacuna'
+                  }
+        widgets = {'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+                   'sexo': forms.TextInput(attrs={'class': 'form-control'}),
+                   'edad_aproximada': forms.TextInput(attrs={'class': 'form-control'}),
+                   'fecha_de_rescate': forms.TextInput(attrs={'class': 'form-control'}),
+                   'raza': forms.TextInput(attrs={'class': 'form-control'}),
+                   'comentarios': forms.TextInput(attrs={'class': 'form-control'}),
+                   'persona': forms.Select(attrs={'class': 'form-control'}),
+                   'vacuna': forms.CheckboxSelectMultiple(),
+                   }
