714842da47fb4b879ab424cc61cd39ef6cdd66d8
alfonso <@lfareiza1989>
2021-04-15 20:20:46 -0300

    Nueva view para editar mascota.

--- refugio/apps/mascota/views.py
+++ refugio/apps/mascota/views.py
@@ -12,19 +12,31 @@ def index_mascota(request):
     return render(request, 'mascota/mascota.html')
 
 
-def mascota_view(request):
+def mascota_create(request):
     if request.method == 'POST':
         form = MascotaForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse('index'))  # Va a la app mascota y busca la url que tenga name=index
+            return HttpResponseRedirect(reverse('mascota:index'))
         else:
             return render(request, 'mascota/mascota_form.html', {'form': form}, status=400)
 
     form = MascotaForm()
-    return render(request, 'mascota/mascota_form.html', {'form': form})
+    return render(request, 'mascota/mascota_form.html', context={'form': form})
 
 
 def mascota_list(request):
-    mascota = Mascota.objects.all()
+    mascota = Mascota.objects.all().order_by('id')
     return render(request, 'mascota/mascota_list.html', context={'mascotas': mascota})
+
+
+def mascota_edit(request, id_mascota):
+    mascota = Mascota.objects.get(id=id_mascota)
+    if request.method == 'GET':
+        form = MascotaForm(instance=mascota)
+    else:
+        form = MascotaForm(request.POST, instance=mascota)
+        if form.is_valid():
+            form.save()
+        return HttpResponseRedirect(reverse('mascota:mascota_listar'))
+    return render(request, 'mascota/mascota_form.html', context={'form': form})
