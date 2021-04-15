4579952473314916c4077f0787ddfe7b4628bfba
alfonso <@lfareiza1989>
2021-04-15 20:20:27 -0300

    Agregado app_name al comienzo del archivo como una buena práctica y nueva view para editar mascota.

--- refugio/apps/mascota/urls.py
+++ refugio/apps/mascota/urls.py
@@ -4,8 +4,10 @@ from django.urls import path
 # Aqui deben ir todas las urls de la aplicación
 from refugio.apps.mascota import views
 
+app_name = 'mascota'
 urlpatterns = [
     path('', views.index_mascota, name='index'),
-    path('nuevo/', views.mascota_view, name='mascota_crear'),
-    path('listar/', views.mascota_list, name='mascota_listar')
+    path('nuevo/', views.mascota_create, name='mascota_crear'),
+    path('listar/', views.mascota_list, name='mascota_listar'),
+    path('editar/<int:id_mascota>/', views.mascota_edit, name='mascota_editar')
 ]
