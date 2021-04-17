f7bfae6ecbb160d80a14cb3a0be8dfd2376a0e15
alfonso <@lfareiza1989>
2021-04-17 19:47:35 -0300

    Agregadas urls para nuevas views, agregar, eliminar, actualizar y ver solicitudes de adopción.

--- refugio/apps/adopcion/urls.py
+++ refugio/apps/adopcion/urls.py
@@ -6,5 +6,9 @@ from refugio.apps.adopcion import views
 
 app_name = 'adopcion'
 urlpatterns = [
-    path('', views.index_adopcion, name='home')
+    path('', views.index_adopcion, name='home'),
+    path('solicitud/listar/', views.SolicitudList.as_view(), name='solicitud_listar'),
+    path('solicitud/nuevo/', views.SolicitudCreate.as_view(), name='solicitud_crear'),
+    path('solicitud/editar/<int:pk>/', views.SolicitudUpdate.as_view(), name='solicitud_editar'),
+    path('solicitud/eliminar/<int:pk>/', views.SolicitudDelete.as_view(), name='solicitud_eliminar')
 ]
