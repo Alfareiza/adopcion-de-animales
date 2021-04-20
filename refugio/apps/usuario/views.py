305502185c872a284a45345a9d91385e2e0c4c29
alfonso <@lfareiza1989>
2021-04-20 10:57:17 -0300

    View para registrar usuario basándose en Clase.

--- refugio/apps/usuario/views.py
+++ refugio/apps/usuario/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from refugio.apps.usuario.forms import RegistroForm
+
+
+class RegistroUsuario(CreateView):
+    model = User
+    template_name = "usuario/registrar.html"
+    form_class = RegistroForm
+    success_url = reverse_lazy('mascota:mascota_listar')
