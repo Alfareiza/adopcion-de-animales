c666222ecbe5128a5f4ccfbe57848f37668370f2
alfonso <@lfareiza1989>
2021-04-21 13:49:57 -0300

    Corregido problema en paréntesis

--- refugio/apps/adopcion/urls.py
+++ refugio/apps/adopcion/urls.py
@@ -8,8 +8,8 @@ from refugio.apps.adopcion import views
 app_name = 'adopcion'
 urlpatterns = [
     path('', views.index_adopcion, name='home'),
-    path('solicitud/listar/',  login_required(views.SolicitudList.as_view()(), name='solicitud_listar'),
-    path('solicitud/nuevo/',  login_required(views.SolicitudCreate.as_view()(), name='solicitud_crear'),
-    path('solicitud/editar/<int:pk>/',  login_required(views.SolicitudUpdate.as_view()(), name='solicitud_editar'),
-    path('solicitud/eliminar/<int:pk>/',  login_required(views.SolicitudDelete.as_view()(), name='solicitud_eliminar')
+    path('solicitud/listar/',  login_required(views.SolicitudList.as_view()), name='solicitud_listar'),
+    path('solicitud/nuevo/',  login_required(views.SolicitudCreate.as_view()), name='solicitud_crear'),
+    path('solicitud/editar/<int:pk>/',  login_required(views.SolicitudUpdate.as_view()), name='solicitud_editar'),
+    path('solicitud/eliminar/<int:pk>/',  login_required(views.SolicitudDelete.as_view()), name='solicitud_eliminar'),
 ]
