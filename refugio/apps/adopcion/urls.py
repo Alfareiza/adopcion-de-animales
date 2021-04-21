ee8abc7b9f2e2b85f9e61403d31d125c9ebcd90b
alfonso <@lfareiza1989>
2021-04-21 13:46:49 -0300

    login_required para acceder a las vistas de la app adopcion

--- refugio/apps/adopcion/urls.py
+++ refugio/apps/adopcion/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 # Aqui deben ir todas las urls de la aplicación
@@ -7,8 +8,8 @@ from refugio.apps.adopcion import views
 app_name = 'adopcion'
 urlpatterns = [
     path('', views.index_adopcion, name='home'),
-    path('solicitud/listar/', views.SolicitudList.as_view(), name='solicitud_listar'),
-    path('solicitud/nuevo/', views.SolicitudCreate.as_view(), name='solicitud_crear'),
-    path('solicitud/editar/<int:pk>/', views.SolicitudUpdate.as_view(), name='solicitud_editar'),
-    path('solicitud/eliminar/<int:pk>/', views.SolicitudDelete.as_view(), name='solicitud_eliminar')
+    path('solicitud/listar/',  login_required(views.SolicitudList.as_view()(), name='solicitud_listar'),
+    path('solicitud/nuevo/',  login_required(views.SolicitudCreate.as_view()(), name='solicitud_crear'),
+    path('solicitud/editar/<int:pk>/',  login_required(views.SolicitudUpdate.as_view()(), name='solicitud_editar'),
+    path('solicitud/eliminar/<int:pk>/',  login_required(views.SolicitudDelete.as_view()(), name='solicitud_eliminar')
 ]
