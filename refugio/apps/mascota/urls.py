40f10792679d25d0dd04d7cbf7eae80ae59827a0
alfonso <@lfareiza1989>
2021-04-21 13:45:55 -0300

    login_required para acceder a las vistas de la app mascota

--- refugio/apps/mascota/urls.py
+++ refugio/apps/mascota/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 # Aqui deben ir todas las urls de la aplicación
@@ -7,8 +8,8 @@ from refugio.apps.mascota import views
 app_name = 'mascota'
 urlpatterns = [
     path('', views.index_mascota, name='index'),
-    path('nuevo/', views.MascotaCreate.as_view(), name='mascota_crear'),
-    path('listar/', views.MascotaList.as_view(), name='mascota_listar'),
-    path('editar/<int:pk>/', views.MascotaUpdate.as_view(), name='mascota_editar'),
-    path('eliminar/<int:pk>/', views.MascotaDelete.as_view(), name='mascota_eliminar')
+    path('nuevo/', login_required(views.MascotaCreate.as_view()), name='mascota_crear'),
+    path('listar/', login_required(views.MascotaList.as_view()), name='mascota_listar'),
+    path('editar/<int:pk>/', login_required(views.MascotaUpdate.as_view()), name='mascota_editar'),
+    path('eliminar/<int:pk>/', login_required(views.MascotaDelete.as_view()), name='mascota_eliminar')
 ]
