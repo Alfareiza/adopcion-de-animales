e8346515c4276c576c8dc336686b2848091a2cf0
alfonso <@lfareiza1989>
2021-04-21 16:09:23 -0300

    Agregando clase de css form-control para mejor visualización.

--- refugio/apps/usuario/forms.py
+++ refugio/apps/usuario/forms.py
@@ -6,8 +6,13 @@ from refugio.apps.adopcion.models import Persona, Solicitud
 
 
 class RegistroForm(UserCreationForm):
-
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email']
-        labels = {'username': "Nombre de usuario", 'first_name': "Nombres", 'last_name': "Apellidos", 'e-mail': "E-mail"}
+        fields = ['username', 'first_name', 'last_name', 'email', 'password']
+        labels = {'username': "Nombre de usuario", 'first_name': "Nombres", 'last_name': "Apellidos",
+                  'email': "E-mail"}
+        widgets = {'username': forms.TextInput(attrs={'class': 'form-control'}),
+                   'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+                   'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+                   'email': forms.EmailInput(attrs={'class': 'form-control'}),
+                   'password': forms.PasswordInput(attrs={'class': 'form-control'})}
